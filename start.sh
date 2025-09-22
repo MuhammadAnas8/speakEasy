a0649f57a697e90d518fec11402704a11bd9c031
MuhammadAnas8 <Muhammadanas.tech@gmail.com>
2025-09-23 00:43:32 +0500

    add: file to redirect rasa

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+apt-get update && apt-get install -y python3 python3-pip
 cd rasa_backend
+python3 -m pip install -r requirements.txt
 python3 -m rasa run --enable-api --cors "*" --interface 0.0.0.0 --port $PORT --model models/current-model.tar.gz --log-level INFO
