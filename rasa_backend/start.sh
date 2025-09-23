e4317428ad68417a4236f259d1844b59f5e81b47
MuhammadAnas8 <Muhammadanas.tech@gmail.com>
2025-09-24 00:45:51 +0500

    fix: start.sh for railway deployment

--- rasa_backend/start.sh
+++ rasa_backend/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-rasa run --enable-api --cors "*" --interface 0.0.0.0 --port ${PORT:-8000} --model models/current-model.tar.gz
-# Note: we specify the model path here so it works on platforms like Railway
-# that set $PORT for you. You can also hardcode a port if you prefer.   
+pip install -r requirements.txt
+rasa run --enable-api --cors "*" --interface 0.0.0.0 --port $PORT --model models/current-model.tar.gz
